eb5c179f65b50344ef77b6524d00f9a21d492feb
NATHAN-PC\Nathan <hobosanchez88@gmail.com>
2017-10-13 16:52:38 -0400

    i deleted the dick project. i put comments around things in the scripts that are worth noting

---
+++ GroupGame/Assets/Scripts/CamScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+    public GameObject player;
+
+    private Vector3 offset;
+    
+	void Start () {
+        offset = player.transform.position - transform.position;
+        transform.LookAt(player.transform);
+	}
+	
+	void LateUpdate () {
+        transform.position = player.transform.position - offset;
+        transform.LookAt(player.transform);
+    }
+}
---
+++ GroupGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+//  use these to calibrate joystick input
+    public float xMin, xMax, yMin, yMax;
+//  in the inspector window
+
+    void Start ()
+    {
+
+	}
+
+	// Update
+	void FixedUpdate ()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        if(hor <= xMax && hor >= xMin)
+            hor = 0;
+
+        float ver = Input.GetAxis("Vertical");
+        if (ver <= yMax && ver >= yMin)
+            ver = 0;
+
+    //  un-comment this if you want to mess with the axis and need a
+    //  visual value in the console window
+        //Debug.Log("left/right: " + hor + "\nforward/back: " + ver);
+
+        Vector3 move = new Vector3(-hor, 0.0f, ver);
+        transform.eulerAngles = new Vector3(0, Mathf.Atan2(-hor, ver) * 180 / Mathf.PI, 0);
+        transform.position += move * speed * Time.deltaTime;
+	}
+}
