b86b324c3dd03b6536b035824fc725b22bfc24ad
SLCTECH\ndrew <hobosanchez88&gmail.com>
2017-11-24 11:10:22 -0500

    stuff script info

---
+++ GroupGame/Assets/Scripts/stuff.cs
@@ -0,0 +1,28 @@
+﻿//scripts we need
+/*
+ * camera control
+ *  - basic camera controls to look around.
+ *  
+ * controller control
+ *  - button mapping and controller actions
+ *  
+ * collision detection
+ *  - attack range and collisions between characters and objects
+ *  
+ * game/spawn controller
+ *  - all game related coding to make it run
+ *  - music control
+ *  - menus and stuff
+ *  - damage, hp, mp?, speed, waves, etc...
+ *  - spawn locations for enemies and players
+ *  - enemy type
+ *  - enemy nav control(determine an enemies target based on distance)
+ *  - score keeping
+ *  
+ * animation control
+ *  - pretty straight forward...
+ *  
+ * scene control
+ *  - scene switching 
+ * 
+*/
