6febefe11f0add53c98cc41151b3003892a2a5dc
SLCTECH\ndrew <hobosanchez88&gmail.com>
2017-12-01 10:35:28 -0500

    new scripts

---
+++ GroupGame/Assets/Scripts/SpawnControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControl : MonoBehaviour {
+    public GameObject[] spawnLocation;
+    public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+        Instantiate(enemyPrefab, spawnLocation[0].transform);
+        Instantiate(enemyPrefab, spawnLocation[1].transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
