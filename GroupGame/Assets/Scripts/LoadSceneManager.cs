b5dbaa1212d8eb4c5b11f91bdcf363eb0e61a21f
SLCTECH\jlee <thevanica@gmail.com>
2017-12-01 10:00:37 -0500

    Added SceneManager, organized folder structure

---
+++ GroupGame/Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour {
+
+    // Use this for initialization
+
+    public static LoadSceneManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LoadSceneManager>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject("LoadSceneManager");
+                    instance = go.AddComponent<LoadSceneManager>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+
+    private static LoadSceneManager instance = null;
+
+
+   public static LoadSceneManager GetInstance()
+    {
+        return Instance;
+    }
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ GroupGame/Assets/Scripts/Main/GamePlayManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LoadSceneManager.Instance.LoadScene("Lobby");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
