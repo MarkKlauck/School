b4d268712e0382663d245fab25671fb987f4d730
NATHAN-PC\Nathan <hobosanchez88@gmail.com>
2017-12-09 12:54:20 -0500

    tweaked the main game scene, added a score hud for each screen and mapped 4 controllers in the input manager

--- GroupGame/Assets/Scripts/CamControl.cs
+++ GroupGame/Assets/Scripts/CamControl.cs
@@ -3,30 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamControl : MonoBehaviour {
+    private float yOffset;
+
     private Transform target;
     private int plyrNum;
     private string joy;
 	// Use this for initialization
 	void Start () {
-        GetPlayerNumber();
-        joy = "Player" + plyrNum + " Rightx";
         target = gameObject.transform.parent.GetComponentInParent<Transform>();
-        Debug.Log(target.gameObject.name);
+        yOffset = 1.5f;
+        plyrNum = GetPlayerNumber();
+        joy = "Player" + plyrNum + " Rightx";
+        
+       // Debug.Log(target.gameObject.name);
+       // Debug.Log(joy);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetAxis(joy)!=0)
-        {
-            float movement = Input.GetAxis(joy);
-            transform.Translate(Vector3.right * movement * Time.deltaTime * 3.0f);
-        }
-        transform.LookAt(target);
+        
+         if(Input.GetAxis(joy)!=0)
+         {
+             float movement = Input.GetAxis(joy);
+             transform.Translate(Vector3.right * movement * Time.deltaTime * 3.0f);
+         }
+        transform.LookAt(target.position + new Vector3(0, yOffset, 0));
 	}
 
-    void GetPlayerNumber()
+    public int GetPlayerNumber()
     {
         PlayerControl pc = gameObject.transform.parent.GetComponentInParent<PlayerControl>();
-        plyrNum = pc.GetPlayerNumber();
+        return pc.GetPlayerNumber();
     }
 }
