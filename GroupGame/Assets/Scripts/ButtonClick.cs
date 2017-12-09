426b0d7d779cd55cf92f247cd6e35df0538a72d7
NATHAN-PC\Nathan <hobosanchez88@gmail.com>
2017-12-09 17:21:40 -0500

    started a base layout for the lobby scene

---
+++ GroupGame/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+    private LobbyScript lb;
+	// Use this for initialization
+	void Start () {
+        lb = GameObject.Find("LobbyManager").GetComponent<LobbyScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnClick()
+    {
+        lb.AddPlayer();
+        gameObject.SetActive(false);
+    }
+}
