a83119c664915bb05ebb2f1e05716ddc13ec5295
SLCTECH\jlee <thevanica@gmail.com>
2017-12-21 13:34:04 -0500

    Added Character Screen in the lobby scene

--- GroupGame/Assets/Scripts/LobbyScript.cs
+++ GroupGame/Assets/Scripts/LobbyScript.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LobbyScript : MonoBehaviour {
+public class LobbyScript : MonoBehaviour
+{
     //public Image[] images;
     public Button[] buttons;
-    public Image imgPrefab;
+    public RawImage imgPrefab;
     //public Button btnPrefab;
 
+    public Camera[] character_cams;
     private int playerNum = 1;
 
     private float newWidth = Screen.width / 2 - 10;
     private float newHeight = Screen.height / 2 - 10;
     private List<int> nums;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         PlayerPrefs.SetInt("player1", 0);
         nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };// 12, 13, 14, 15, 16 };
         SetScene();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         // need to configure input manager to reflect player joy number
         // modify cam script to take player joy number instead of player number
@@ -42,13 +46,13 @@ public class LobbyScript : MonoBehaviour {
         }
         if (PlayerPrefs.GetInt("player1") != 0)
         {
-            if (Input.GetKey("joystick "+ PlayerPrefs.GetInt("player1") + " button 7"))
+            if (Input.GetKey("joystick " + PlayerPrefs.GetInt("player1") + " button 7"))
             {
                 LoadSceneManager lsm = GameObject.Find("start game").GetComponent<LoadSceneManager>();
                 lsm.LoadSceneByButton("Main");
             }
         }
-	}
+    }
 
     public void SetScene()
     {
@@ -59,14 +63,21 @@ public class LobbyScript : MonoBehaviour {
             new Vector2(0, 0),
             new Vector2(Screen.width/2, 0),
         };
-        for (int i = 0;i<4;i++)
+        for (int i = 0; i < 4; i++)
         {
             GameObject canv = GameObject.Find("Canvas");
-            Image img = Instantiate(imgPrefab, bkgLoc[i], Quaternion.identity) as Image;
-            img.transform.parent = GameObject.Find("BKGPanels").transform;
+            RawImage img = Instantiate(imgPrefab, bkgLoc[i], Quaternion.identity) as RawImage;
+            img.transform.SetParent(GameObject.Find("BKGPanels").transform);
             img.rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
             img.rectTransform.anchoredPosition = bkgLoc[i];
             buttons[i].gameObject.SetActive(true);
+
+
+            RenderTexture rt = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+            rt.Create();
+            character_cams[i].targetTexture = rt;
+            img.texture = rt;
+
         }
     }
 
