244392aa0bc56b9fa708695a4de0bc59590059d6
DESKTOP-BBE95OP\A <thevanica@gmail.com>
2017-12-22 16:24:47 -0500

    added blood effect

---
+++ GroupGame/Assets/Scripts/BloodImage.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BloodImage : MonoBehaviour {
+
+    public Player player;
+    public float fade_start_percent = 0.2f;
+    public bool blood_start = false;
+    private Image image;
+    public float fPercent = 1.0f;
+    public float fade_accel = 3.0f;
+    // Use this for initialization
+    void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(blood_start)
+        {
+           // float fPercent = (float)player.GetHP() / (float)player.GetMaxHP();
+            if(fPercent <= fade_start_percent)
+            {
+                float alpha;
+                if (fPercent == 0.0f)
+                {
+                    alpha = 1.0f;
+                }
+                else
+                {
+                    // 5 steps
+                    // 20% -> 15% -> 10% -> 5% -> 0
+                    //  1  ->  2  ->  3  -> 4  -> 5 times
+                    float fade_step = ((fade_start_percent - fPercent) / 0.05f) + 1.0f;
+                    float fade_speed = Mathf.Pow(fade_accel, fade_step);
+                    alpha = (Mathf.Sin(Time.time * fade_speed) * 0.5f + 0.5f);
+                }
+               
+                image.color = new Color(1, 1, 1, alpha);
+            }
+            else
+            {
+                blood_start = false;
+                image.color = new Color(1, 1, 1, 0);
+            }
+           
+        }
+        
+	}
+
+}
--- GroupGame/Assets/Scripts/Player.cs
+++ GroupGame/Assets/Scripts/Player.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    private int hp, damage;
+    private int hp, maxhp, damage;
 
     //collision stuff here?
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
+        SetMaxHP(100);
         SetHp(100);
         SetDamage(40);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
     //use to set hp to a specified amount
     public void SetHp(int amnt)
     {
         hp = amnt;
+    }
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public void SetMaxHP(int amnt)
+    {
+        maxhp = amnt;
+    }
+
+    public int GetMaxHP()
+    {
+        return maxhp;
     }
     //use to increase/decrease hp
     public void TakeDamage(int n)
