b3807abd93ddd3b24bb75f4eb7b1ba33816cea69
SLCTECH\jlee <thevanica@gmail.com>
2018-01-11 13:03:58 -0500

    Added Credit Scene

---
+++ GroupGame/Assets/Scripts/MovingText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MovingText : MonoBehaviour {
+    private Text creditText;
+
+    public float offset = 0.5f;
+    // Use this for initialization
+    void Start()
+    {
+        creditText = GetComponent<Text>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        creditText.rectTransform.anchoredPosition = new Vector2(creditText.rectTransform.anchoredPosition.x, creditText.rectTransform.anchoredPosition.y + offset);
+    }
+}
